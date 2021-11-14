2b21d63e1a31be90f8faafcfea3b696f79286c4e
Onkar Birajdar <67547781+Onkar-Birajdar@users.noreply.github.com>
2021-11-14 23:25:02 +0530

    Update 038_Middle_Of_The_Linked_List.cpp

--- Onkar Birajdar/Linked List/038_Middle_Of_The_Linked_List.cpp
+++ Onkar Birajdar/Linked List/038_Middle_Of_The_Linked_List.cpp
@@ -7,16 +7,6 @@ If there are two middle nodes, return the second middle node.
 Prob. Link: https://leetcode.com/problems/middle-of-the-linked-list/
 */
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution
 {
 public:
